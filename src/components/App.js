a1d0902e37eb4808083302c5c585115235b46df6
trevsm <trevorsm@pm.me>
2022-12-25 22:36:18 -0700

    working example of child_process

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const App = () => {
-  return <div>Hello World.</div>
+  useEffect(() => {
+    // execute ls command using child_process
+    const { exec } = require("child_process")
+    exec("ls", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+    })
+  }, [])
+  return <div>Hello World</div>
 }
 
 export default App
--- src/electron/dev.js
+++ src/electron/dev.js
@@ -12,7 +12,15 @@ const createWindow = () => {
   })
 
   window.loadFile("build/index.html")
+  window.on("closed", () => (window = null))
 }
 
 app.whenReady().then(createWindow)
+
 app.on("window-all-closed", () => app.quit())
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
